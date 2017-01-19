ac7aff06801cffaa08d4489a46f0876c617aff37
Sao Thien Hat <saothienhat@gmail.com>
2017-01-19 17:43:53 +0700

    Update code for FFMPEG options
    Update code for FFMPEG options

--- ffmpegjavademo/src/main/java/bk/saothienhat/ffmpegjavademo/App.java
+++ ffmpegjavademo/src/main/java/bk/saothienhat/ffmpegjavademo/App.java
@@ -1,6 +1,8 @@
 package bk.saothienhat.ffmpegjavademo;
 
 import bk.saothienhat.ffmpegjavademo.model.AppConfig;
+import bk.saothienhat.ffmpegjavademo.model.AppConst;
+import bk.saothienhat.ffmpegjavademo.model.FFMPEGOptionType;
 import bk.saothienhat.ffmpegjavademo.model.FileType;
 import bk.saothienhat.ffmpegjavademo.utils.Logger;
 
@@ -13,18 +15,52 @@ public class App
         long startTime = System.currentTimeMillis();
         
         String inputFilePath = "D://your_file.avi";
-        String finalOutputFile = "D://Final_Output_File.mp4";
+        String convertedMp4OutputFile = "D://Converted_Media_File.mp4";
+        String compressedAVIOutputFile = "D://Compressed_Avi_Media_File.avi";
+        String finalMp4OutputFile = "D://Final_Mp4_File.mp4";
+        String finalAviOutputFile = "D://Final_AVI_File.avi";
+        
         
         FfmpegHandler ffmpegHandler = new FfmpegHandler();
         ffmpegHandler.setFfmpegPath(AppConfig.FFMPEG_4_WINDOW_PATH);       
+
+        /* Get video file info */
+        // ffmpegHandler.getVideoFileInfo(inputFilePath, false);  
         
         
-//        ffmpegHandler.compressMediaFile(FileType.AVI, inputFilePath, outputFilePath);        
-//        ffmpegHandler.getVideoFileInfo(inputFilePath, false);        
+        /*
+         * Convert AVI => MP4 and reduce MP4 output file
+         *  Ref: https://andre.blue/blog/converting-avi-to-mp4-with-ffmpeg/
+         */
+        String audioBitrates = "128k";
+        String videoBitrates = "120k";
+        StringBuilder convertOptionBuilder = new StringBuilder(); 
+        // convertOptions: -acodec libfaac -b:a 128k -vcodec mpeg4 -b:v 120k -flags +aic+mv4
+        convertOptionBuilder.append(FFMPEGOptionType.ACODEC.getOption());
+        convertOptionBuilder.append(AppConst.SPACE);
+        convertOptionBuilder.append(FFMPEGOptionType.LIBFAAC.getOption());
+        convertOptionBuilder.append(AppConst.SPACE);
+        convertOptionBuilder.append(FFMPEGOptionType.BA.getOption());
+        convertOptionBuilder.append(AppConst.SPACE);
+        convertOptionBuilder.append(audioBitrates);
+        convertOptionBuilder.append(AppConst.SPACE);
+        convertOptionBuilder.append(FFMPEGOptionType.VCODEC.getOption());
+        convertOptionBuilder.append(AppConst.SPACE);
+        convertOptionBuilder.append(FFMPEGOptionType.MPEG4.getOption());
+        convertOptionBuilder.append(AppConst.SPACE);
+        convertOptionBuilder.append(FFMPEGOptionType.BV.getOption());
+        convertOptionBuilder.append(AppConst.SPACE);
+        convertOptionBuilder.append(videoBitrates);
+        convertOptionBuilder.append(AppConst.SPACE);
+        convertOptionBuilder.append(FFMPEGOptionType.FLAGS.getOption());
+        convertOptionBuilder.append(AppConst.SPACE);
+        convertOptionBuilder.append("+" + FFMPEGOptionType.AIC.getOption() + "+" + FFMPEGOptionType.MV4.getOption()); // +aic+mv4  
+        String convertOptions = convertOptionBuilder.toString();
+//        ffmpegHandler.convert(FFMPEGOptionType.I.getOption(), inputFilePath, convertOptions, finalMp4OutputFile);
       
-      long stopTime = System.currentTimeMillis();
-      long exeTime = stopTime - startTime;
-      Logger.log("**** App.java: To finish all of them, it takes: " + (exeTime / 1000) + " (s) !!!");
+        long stopTime = System.currentTimeMillis();
+        long exeTime = stopTime - startTime;
+        Logger.log("**** App.java: To finish all of them, it takes: " + (exeTime / 1000) + " (s) !!!");
 
     }
     
--- ffmpegjavademo/src/main/java/bk/saothienhat/ffmpegjavademo/model/AppConst.java
+++ ffmpegjavademo/src/main/java/bk/saothienhat/ffmpegjavademo/model/AppConst.java
@@ -1,16 +1,15 @@
 /*
- * Copyright (c) 2017 Trinh Thanh Binh. All rights reserved.
+ * Copyright (c) 2017 Sao Thien Hat. All rights reserved.
  */
 package bk.saothienhat.ffmpegjavademo.model;
 
 /**
- * @author binhtt
+ * @author Sao Thien Hat
  *
  */
 public class AppConst {
-    public static final String FFMPEG_OPTION_I = "-i";
-    public static final String FFMPEG_OPTION_CONVERT_EMPTY = "";
-    // The -hide_banner option is used to hide a copyright notice shown my ffmpeg, such as build options and library versions. This option can be used to suppress printing this information.
-    public static final String FFMPEG_OPTION_HIDE_BANNER = "-hide_banner";
+       
+    
+    public static final String SPACE = " ";
 
 }
